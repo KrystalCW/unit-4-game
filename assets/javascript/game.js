3e18546470c2c92915013216108d9b79921da698
KrystalCW <krystal.wiederrich@gmail.com>
2019-02-15 22:27:41 -0800

    finalized design and functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,8 +15,21 @@ function startGame() {
     crystalTwoScore = Math.floor( Math.random() * 10) + 1;
     crystalThreeScore = Math.floor( Math.random() * 10) + 1;
     crystalFourScore = Math.floor( Math.random() * 10) + 1;
+    checkDuplicates()
     scores = [];
     $("#current-score").html(0);
+}
+
+function checkDuplicates() {
+    if (crystalTwoScore === crystalOneScore) {
+        crystalTwoScore = Math.floor( Math.random() * 10) + 1;
+    }
+    else if (crystalThreeScore === crystalOneScore || crystalTwoScore) {
+        crystalThreeScore = Math.floor( Math.random() * 10) + 1;
+    }
+    else if (crystalFourScore === crystalOneScore || crystalTwoScore || crystalThreeScore) {
+        crystalFourScore = Math.floor( Math.random() * 10) + 1;
+    }
 }
 
 function gamePlay() {
@@ -24,25 +37,25 @@ function gamePlay() {
         console.log(crystalOneScore);
         scores.push(crystalOneScore);
         $("#current-score").html(arrSum(scores));
-        winLose();
+        setTimeout(winLose, 500);
     })
     $("#crystal2").on("click", function() {
         console.log(crystalTwoScore);
         scores.push(crystalTwoScore);
         $("#current-score").html(arrSum(scores));
-        winLose();
+        setTimeout(winLose, 500);
     })
     $("#crystal3").on("click", function() {
         console.log(crystalThreeScore);
         scores.push(crystalThreeScore);
         $("#current-score").html(arrSum(scores));
-        winLose();
+        setTimeout(winLose, 500);
     })
     $("#crystal4").on("click", function() {
         console.log(crystalFourScore);
         scores.push(crystalFourScore);
         $("#current-score").html(arrSum(scores));
-        winLose();
+        setTimeout(winLose, 500);
     })
 }
 
@@ -51,12 +64,12 @@ function winLose() {
         alert("You won! Good job!");
         winsCounter++;
         $("#wins").html(winsCounter);
-        startGame();
+        setTimeout(startGame, 1000);
     } else if (arrSum(scores) > goal) {
         alert("Sorry, you lose :(");
         lossesCounter++;
         $("#losses").html(lossesCounter);
-        startGame();
+        setTimeout(startGame, 1000);
     }
 }
 
